e9537ba0bb9572ee8fc7e2de61dc0e64b716fe90
rsiritom <ricardo.a.siri@gmail.com>
2020-07-08 01:18:15 -0300

    Updates

---
+++ precache-manifest.7bd0c140ac76c343a3d7414295143070.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d7f6057ca3385fd1ffe1166e854ef02",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "0b95886b1801935cff62",
+    "url": "/myapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/myapp/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myapp/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b95886b1801935cff62",
+    "url": "/myapp/static/js/main.6107cc93.chunk.js"
+  },
+  {
+    "revision": "2413d325ab07ed7ad194",
+    "url": "/myapp/static/js/runtime-main.0abbbebf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
